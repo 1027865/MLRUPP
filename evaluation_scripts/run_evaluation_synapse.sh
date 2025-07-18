b074c826e9402e62724ffcd6303668f41ad08fa4
1027865 <nand.yadav@usd.edu>
2025-07-18 16:45:03 -0500

    Update run_evaluation_synapse.sh

--- evaluation_scripts/run_evaluation_synapse.sh
+++ evaluation_scripts/run_evaluation_synapse.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 DATASET_PATH=../DATASET_Synapse
-CHECKPOINT_PATH=../unetr_pp/evaluation/mlru_pp_synapse_checkpoint
+CHECKPOINT_PATH=../mlru_pp/evaluation/mlru_pp_synapse_checkpoint
 
 export PYTHONPATH=.././
 export RESULTS_FOLDER="$CHECKPOINT_PATH"
 export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_raw/unetr_pp_raw_data/Task02_Synapse
 export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
 
-python ../unetr_pp/run/run_training.py 3d_fullres mlru_pp_trainer_synapse 2 0 -val
+python ../mlru_pp/run/run_training.py 3d_fullres mlru_pp_trainer_synapse 2 0 -val
