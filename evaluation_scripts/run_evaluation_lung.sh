7e2ef0fd253c39fdaa812ccdeb0cf7e944c32500
1027865 <nand.yadav@usd.edu>
2025-07-18 16:44:33 -0500

    Update run_evaluation_lung.sh

--- evaluation_scripts/run_evaluation_lung.sh
+++ evaluation_scripts/run_evaluation_lung.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 DATASET_PATH=../DATASET_Lungs
-CHECKPOINT_PATH=../unetr_pp/evaluation/unetr_pp_lung_checkpoint
+CHECKPOINT_PATH=../mlru_pp/evaluation/mlru_pp_lung_checkpoint
 
 export PYTHONPATH=.././
 export RESULTS_FOLDER="$CHECKPOINT_PATH"
 export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_raw/unetr_pp_raw_data/Task06_Lung
 export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
 
-python ../unetr_pp/run/run_training.py 3d_fullres unetr_pp_trainer_lung 6 0 -val
+python ../mlru_pp/run/run_training.py 3d_fullres mlru_pp_trainer_lung 6 0 -val
